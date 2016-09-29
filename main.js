4ae2232eb0fb54055fd84583d91da871c0cc5327
Taylor Britton <k.taylor.britton@gmail.com>
2016-09-28 18:38:04 -0700

    cleaned up a little bit of js code

--- main.js
+++ main.js
@@ -46,24 +46,42 @@ var wdi_24_names = [
   {
     name: 'Taylor',
     picture: 'https://puppydogweb.com/wp-content/uploads/2015/05/St._Bernard_puppy.jpg'
+  },
+  {
+    name: 'Mike',
+    picture: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTo5uBzRGUa13cNSU4QMGBnJD4D8CYvmhLOQV7iYDANzy23TOrqtw'
+  },
+  {
+    name: 'Kate',
+    picture: 'https://s-media-cache-ak0.pinimg.com/564x/09/59/08/09590801ade7f0927827763692e5bc30.jpg'
+  },
+  {
+    name: 'Mark',
+    picture: 'https://www.royalcanin.com/~/media/Royal-Canin/Product-Categories/dog-puppy-landing-hero.ashx'
+  },
+  {
+    name: 'Loris',
+    picture: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSM-F5huVpQ9ANTdV924g_kYHV5zxtdBPjB_xXWE5xx6YP2lOKE'
   }
 ];
 
-var called = [];
 
 function randomName() {
   // generate random number between 0 and the number of people in the class - 1
   var number = Math.floor(Math.random() * wdi_24_names.length);
   // find the name at that random index
   var person = wdi_24_names[number];
+  // array of called names begins empty
+  var called = [];
 
+  // change the page to reflect the
   $('#displayName').html(person.name);
   $('#question').attr('hidden', true);
-  $('#headshot').attr('hidden', false);
-  $('#headshot').attr('src', person.picture);
+  $('#puppy-pic').attr('hidden', false);
+  $('#puppy-pic').attr('src', person.picture);
   console.log(person.name);
 
-  // add the name to the called array and splice it from the orginal array
+  // add the name to the called array and remove it from the orginal array
   called.push(person);
   wdi_24_names.splice(wdi_24_names.indexOf(person), 1);
 
